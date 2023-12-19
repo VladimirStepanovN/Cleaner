c5bf51ac7218f86ccbd7da59d52178931b9dfd3b
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-12-20 01:30:42 +0500

    Задание 3 (HW-03)

---
+++ Cleaner.cs
@@ -0,0 +1,61 @@
+﻿namespace Cleaner;
+
+internal class Cleaner
+{
+    internal (double sizeFolderBeforeClean, double sizeFreeSpaceAfterDelete, int countDeletedFiles) result;
+    internal (double sizeFolderBeforeClean, double sizeFreeSpaceAfterDelete, int countDeletedFilesint) Clean(string Path, bool isSubfolder = false)
+    {
+        try
+        {
+            var directory = new DirectoryInfo(Path);
+            if (directory.Exists)
+            {
+                foreach (DirectoryInfo dir in directory.GetDirectories())
+                {
+                    try
+                    {
+                        Clean(dir.FullName, true);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Недопустимое действие или недостаточно прав на выполнение для {dir.FullName}.");
+                        continue;
+                    }
+                }
+                FileInfo[] files = directory.GetFiles();
+                if (files.Length > 0)
+                {
+                    foreach (FileInfo file in files)
+                    {
+                        try
+                        {
+                            result.sizeFolderBeforeClean += file.Length;
+                            if (file.LastAccessTime.AddMinutes(30.0) < DateTime.Now)
+                            {
+                                result.sizeFreeSpaceAfterDelete += file.Length;
+                                result.countDeletedFiles++;
+                                file.Delete();
+                            }
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            Console.WriteLine($"Недопустимое действие или недостаточно прав на выполнение для {file.FullName}.");
+                            continue;
+                        }
+                    }
+                }
+                // если папка пустая и вложенная (признак isSubfolder), то удаляем
+                if (directory.GetFiles().Length == 0 && directory.GetDirectories().Length == 0 && isSubfolder) directory.Delete();
+            }
+            else
+            {
+                Console.WriteLine($"Указанный каталог {Path} не существует");
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        return result;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Cleaner
+﻿namespace Cleaner;
+
+internal class Program
 {
-    internal class Program
+    internal static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]))
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Cleaner cleaner = new();
+                var (sizeFolderBeforeClean, sizeFreeSpaceAfterDelete, countDeletedFilesint) = cleaner.Clean(args[0]);
+                Console.WriteLine($"Исходный размер папки: {sizeFolderBeforeClean} байт");
+                Console.WriteLine($"Удалено: {countDeletedFilesint} файлов");
+                Console.WriteLine($"Освобождено: {sizeFreeSpaceAfterDelete} байт");
+                Console.WriteLine($"Текущий размер папки: {sizeFolderBeforeClean - sizeFreeSpaceAfterDelete}");
+                Console.ReadKey();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Недопустимая часть пути к каталогу.");
+            }
+            catch (PathTooLongException)
+            {
+                Console.WriteLine("Путь или имя файла превышает максимальную длину, определенную системой.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Путь к каталогу не указан.");
         }
     }
 }
